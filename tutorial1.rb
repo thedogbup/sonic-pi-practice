8aa59bfca030461cd6edf46afcdc4db81306cf69
bup <50534745+thedogbup@users.noreply.github.com>
2020-11-29 00:14:35 -0500

    Update tutorial1.rb

--- tutorial1.rb
+++ tutorial1.rb
@@ -1,7 +1,6 @@
 # Welcome to Sonic Pi
 
 live_loop :flibble do
-  sample :ambi_choir, rate: 0.1
   sample :bd_tek, rate: 1
   #  sample :bd_haus, rate: 1
   sleep 0.5
@@ -11,7 +10,6 @@ live_loop :guit do
   with_fx :echo, mix: 0.3, phase: 0.25 do
     sample :guit_em9, rate: 0.5
   end
-  sample :guit_em9, rate: -0.25
   sleep 4
 end
 
@@ -19,5 +17,6 @@ live_loop :boom do
   with_fx :reverb, room: 1 do
     sample :bd_boom, amp: 10, rate: 1
   end
+  sample :ambi_choir, rate: 0.1
   sleep 4
 end
